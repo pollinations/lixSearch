3c802cc781a6921cec43ef889b3efec39fb841c0
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-02-13 06:20:36 +0000

    web scraper is a reverse import

--- api/web_scraper.py
+++ api/web_scraper.py
@@ -1,8 +1,3 @@
-"""
-Web Scraper Module - Re-exports fetch_full_text from search module
-This maintains backward compatibility with the existing import structure
-"""
-
 from search import fetch_full_text
 
 __all__ = ['fetch_full_text']
